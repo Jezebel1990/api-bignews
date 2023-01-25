f2685fc66da6b71f90235f7aba5e26d7229144f3
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-25 14:52:33 -0300

    add news

---
+++ src/controllers/news.controller.js
@@ -0,0 +1,35 @@
+import { createService, findAllService } from "../services/news.service.js";
+
+const create = async (req, res) => {
+    try {
+      const { title, text, banner } = req.body;
+
+      if (!title || !banner || !text) {
+        res.status(400).send({
+            message: "Submit all fields for registration",
+        });
+      }
+    await createService({
+        title,
+        text,
+        banner,
+        user: { _id: "63cdce08b77f37d301691240" },
+    });
+
+    res.send(201);
+}catch (err) {
+    res.status(500).send({ message: err.message });
+}
+};
+const findAll = async (req, res) => {
+    const news = await findAllService();
+    if (news.length === 0) {
+        return res.status(400).send({
+            message: "There are no registered news",
+        });
+    }
+    res.send(news);
+
+};
+export { create, findAll };
+    
