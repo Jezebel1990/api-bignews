fef4f4e20e2f4a9753543c55a660cc70a489cc75
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-26 16:20:19 -0300

    add top news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNews } from "../services/news.service.js";
+import { createService, findAllService, countNews, topNewsService } from "../services/news.service.js";
 
 const create = async (req, res) => {
     try {
@@ -22,19 +22,22 @@ const create = async (req, res) => {
     res.status(500).send({ message: err.message });
 }
 };
+
 const findAll = async (req, res) => {
- let {limit, offset} = req.query;
+    try{
+    let {limit, offset} = req.query;
 
-limit = Number(limit);
-offset = Number(offset);
+    limit = Number(limit);
+    offset = Number(offset);
 
-if (!limit) {
-    limit = 5;
-}
+    if (!limit) {
+        limit = 5;
+    }
 
-if (!offset) {
-    offset = 0;
-}
+    if (!offset) {
+        offset = 0;
+    }
+
 const news = await findAllService(offset, limit);
 const total = await countNews();
 const currentUrl = req.baseUrl
@@ -66,11 +69,39 @@ const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${pr
         comments: item.comments,
         name: item.user.name,
         username: item.user.username,
-        userAvatar: item.user.avatar
+        userAvatar: item.user.avatar,
      })),
 
     });
-
+    
+} catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
-export { create, findAll };
+
+const topNews = async (req, res) => {
+    try {
+    const news = await topNewsService();
+
+    if (!news) {
+     return res.status(450).send({ message: "There is no registered post"});
+    }
+res.send({
+    news: {
+        id: news._id,
+        title: news.title,
+        text: news.text,
+        banner: news.banner,
+        likes: news.likes,
+        comments: news.comments,
+        name: news.user.name,
+        username: news.user.username,
+        userAvatar: news.user.avatar,
+    },
+});
+    } catch (err) {
+        res.status(500).send({ message: err.message });
+    }
+};
+export { create, findAll, topNews };
     
