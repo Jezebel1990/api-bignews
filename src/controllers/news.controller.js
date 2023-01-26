381ec6f0115fff68b0d5aa0b816a29fbc82d93a6
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-26 19:38:50 -0300

    search by title

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService, countNews, topNewsService, findByIdService } from "../services/news.service.js";
+import { createService, findAllService, countNews, topNewsService, findByIdService, searchByTitleService } from "../services/news.service.js";
 
 export const create = async (req, res) => {
     try {
@@ -107,7 +107,7 @@ res.send({
     try {
         const {id} = req.params;
 
-     const news = await findByIdService(id);
+        const news = await findByIdService(id);
 
         return res.send({
           news: {
@@ -127,6 +127,30 @@ res.send({
         res.status(500).send({ message: err.message });
     }
  };
-    
+    export const searchByTitle = async (req, res) => {
+        try{
+            const {title} = req.query;
+            const news = await searchByTitleService(title);
+
+        if (news.length === 0 ){
+        return res.status(400).send({ message: "There are no news with this title "});
+}
+    return res.send({
+        results: news.map((item) => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar,
+        })),
+     });
+        } catch (err) {
+            res.status(500).send({ message: err.message });
+        }
+    };
 
 
