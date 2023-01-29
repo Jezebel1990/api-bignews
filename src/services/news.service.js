fb0938f2961ffc61d7ef0dbe747127bedf1b08ac
Jezebel1990 <jezebelguedes@gmail.com>
2023-01-29 00:11:49 -0300

    add comments

--- src/services/news.service.js
+++ src/services/news.service.js
@@ -36,3 +36,20 @@ export const updateService = (id, title, text, banner) => News.findOneAndUpdate(
     
    export const deleteLikeNewsService = (idNews, userId) => News.findOneAndUpdate ({_id: idNews },{$pull: {likes: {userId }}});
     
+   export const addCommentService = (idNews, comment,userId) => {
+    const idComment = Math.floor(Date.now() * Math.random()).toString(36);
+
+    return News.findOneAndUpdate(
+        { _id: idNews },
+        {
+            $push: {
+                comments: { idComment, userId, comment, createdAt: new Date() },
+            },
+        }
+    );
+   };
+   export const deleteCommentService = (idNews, idComment, userId) =>
+  News.findOneAndUpdate(
+    { _id: idNews },
+    { $pull: { comments: { idComment, userId } } }
+  );
